834b06b2efb74d9ba954037425e6fa37ef38bb17
CamiloGato <cm.andres022@hotmail.com>
2024-06-19 18:56:53 -0500

    Update: Creation of simple Network Manager

---
+++ Assets/_Scripts/Player/SamplePlayer.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace _Scripts.Player
+{
+    public class SamplePlayer : NetworkBehaviour
+    {
+        public NetworkVariable<Vector3> position = new NetworkVariable<Vector3>();
+
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner)
+            {
+                Move();
+            }
+        }
+
+        public void Move()
+        {
+            SubmitPositionRequestRpc();
+        }
+
+        [Rpc(SendTo.Server)]
+        private void SubmitPositionRequestRpc(RpcParams rpcParams = default)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            position.Value = randomPosition;
+        }
+
+        static Vector3 GetRandomPositionOnPlane()
+        {
+            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+        }
+
+        void Update()
+        {
+            transform.position = position.Value;
+        }
+    }
+}
---
+++ Assets/_Scripts/SampleNetworkManager.cs
@@ -0,0 +1,69 @@
+﻿using _Scripts.Player;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class HelloWorldManager : MonoBehaviour
+    {
+        private NetworkManager _mNetworkManager;
+
+        void Awake()
+        {
+            _mNetworkManager = GetComponentInParent<NetworkManager>();
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!_mNetworkManager.IsClient && !_mNetworkManager.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+
+                SubmitNewPosition();
+            }
+
+            GUILayout.EndArea();
+        }
+
+        void StartButtons()
+        {
+            if (GUILayout.Button("Host")) _mNetworkManager.StartHost();
+            if (GUILayout.Button("Client")) _mNetworkManager.StartClient();
+            if (GUILayout.Button("Server")) _mNetworkManager.StartServer();
+        }
+
+        void StatusLabels()
+        {
+            var mode = _mNetworkManager.IsHost ?
+                "Host" : _mNetworkManager.IsServer ? "Server" : "Client";
+
+            GUILayout.Label("Transport: " +
+                _mNetworkManager.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+            if (GUILayout.Button("Stop Connection")) _mNetworkManager.Shutdown();
+        }
+
+        void SubmitNewPosition()
+        {
+            if (GUILayout.Button(_mNetworkManager.IsServer ? "Move" : "Request Position Change"))
+            {
+                if (_mNetworkManager.IsServer && !_mNetworkManager.IsClient )
+                {
+                    foreach (ulong uid in _mNetworkManager.ConnectedClientsIds)
+                        _mNetworkManager.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<SamplePlayer>().Move();
+                }
+                else
+                {
+                    NetworkObject playerObject = _mNetworkManager.SpawnManager.GetLocalPlayerObject();
+                    SamplePlayer player = playerObject.GetComponent<SamplePlayer>();
+                    player.Move();
+                }
+            }
+        }
+    }
+}
