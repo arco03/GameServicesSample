29df8dd17587c03d0a3fa52927fe302d6034a12e
CamiloGato <cm.andres022@hotmail.com>
2024-06-28 10:39:38 -0500

    Update: First Approach to Blob Storage

---
+++ Assets/_Systems/Network/Azure/AzureConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Network.Azure
+{
+    [CreateAssetMenu(menuName = "Azure/AzureConfigurations", fileName = "AzureConfigurations", order = 0)]
+    public class AzureConfigurations : ScriptableObject
+    {
+        [Header("Azure Storage")]
+        public string connectionString;
+        public string containerName;
+    }
+}
---
+++ Assets/_Systems/Network/Azure/BlobStorage/StorageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Azure.Storage.Blobs;
+using Cysharp.Threading.Tasks;
+
+namespace Network.Azure.BlobStorage
+{
+    public class StorageManager
+    {
+        private string connectionString = "TuCadenaDeConexion";
+        private string containerName = "nombre-del-contenedor";
+
+        public async UniTask UploadFileAsync(string filePath, string blobName)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            await blobClient.UploadAsync(filePath, true);
+            Console.WriteLine($"Archivo {blobName} subido a Azure Blob Storage.");
+        }
+
+        public async UniTask DownloadFileAsync(string blobName, string downloadFilePath)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            await blobClient.DownloadToAsync(downloadFilePath);
+            Console.WriteLine($"Archivo {blobName} descargado desde Azure Blob Storage a {downloadFilePath}.");
+        }
+    }
+}
