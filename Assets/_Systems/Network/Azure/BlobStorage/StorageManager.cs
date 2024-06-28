20ac9dc8f987bb1cc8cab643fee52b292a204662
CamiloGato <cm.andres022@hotmail.com>
2024-06-28 10:53:43 -0500

    Update: Added IOC to Azure and edited installer

--- Assets/_Systems/Network/Azure/BlobStorage/StorageManager.cs
+++ Assets/_Systems/Network/Azure/BlobStorage/StorageManager.cs
@@ -1,34 +1,38 @@
-﻿using System;
-using Azure.Storage.Blobs;
+﻿using Azure.Storage.Blobs;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Network.Azure.BlobStorage
 {
     public class StorageManager
     {
-        private string connectionString = "TuCadenaDeConexion";
-        private string containerName = "nombre-del-contenedor";
-
+        private readonly AzureConfigurations _configurations;
+        
+        public StorageManager(AzureConfigurations configurations)
+        {
+            _configurations = configurations;
+        }
+        
         public async UniTask UploadFileAsync(string filePath, string blobName)
         {
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_configurations.connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_configurations.containerName);
 
             BlobClient blobClient = containerClient.GetBlobClient(blobName);
 
             await blobClient.UploadAsync(filePath, true);
-            Console.WriteLine($"Archivo {blobName} subido a Azure Blob Storage.");
+            Debug.Log($"File {blobName} uploaded to Azure Blob Storage.");
         }
 
         public async UniTask DownloadFileAsync(string blobName, string downloadFilePath)
         {
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_configurations.connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_configurations.containerName);
 
             BlobClient blobClient = containerClient.GetBlobClient(blobName);
 
             await blobClient.DownloadToAsync(downloadFilePath);
-            Console.WriteLine($"Archivo {blobName} descargado desde Azure Blob Storage a {downloadFilePath}.");
+            Debug.Log($"File {blobName} downloaded from Azure Blob Storage to {downloadFilePath}.");
         }
     }
 }
