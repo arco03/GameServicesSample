8d45eec7049e5e21942effcf96ee4240e52cc920
CamiloGato <cm.andres022@hotmail.com>
2024-06-29 17:44:33 -0500

    Update: Added Movement Packages
    TODO: Finish integration of movement

---
+++ Assets/_Systems/PlayerMovement/TopDownCameraController.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PlayerMovement
+{
+    public class TopDownCameraController : MonoBehaviour
+    {
+        // INPUT
+        public InputActionAsset inputActionAsset;
+        private InputAction _cameraZoomAction;
+        private InputAction _dragAction;
+
+        // CAMERA
+        public Camera playerCamera;
+        public Vector3 cameraPositionOffset = new Vector3(0, 10, 0);
+        public Vector3 cameraRotationOffset = new Vector3(45, 0, 0);
+        public float cameraDampTime = 0.1f;
+        public float offsetDampTime = 0.25f;
+        public float maxOffset = 0.3f;
+        public bool isDraggable = true;
+        public float minCameraHeight = 3,
+            maxCameraHeight = 15,
+            minCameraVertical = 0.5f,
+            maxCameraVertical = 0.5f,
+            cameraZoomSpeed = 15,
+            cameraZoomPower = 5;
+        
+        private float _currentCameraHeight, _cameraHeightTarget, _currentCameraVertical, _cameraVerticalTarget;
+        private Vector3 _forward;
+        private Vector3 _cameraVelocity;
+        private Vector3 _dampedCameraPosition;
+        private Vector2 _dampedOffsetVector;
+        private Vector2 _currentDistanceVectorVelocity;
+        private float _dragAngle;
+        private bool _isDragging;
+
+        private void Awake()
+        {
+            InitializeInputSystem();
+        }
+
+        private void InitializeInputSystem()
+        {
+            _cameraZoomAction = inputActionAsset.FindAction("CameraZoom");
+            _dragAction = inputActionAsset.FindAction("CameraDrag");
+            _cameraZoomAction.Enable();
+            _dragAction.Enable();
+        }
+
+        private void LateUpdate()
+        {
+            if (isDraggable) HandleDrag();
+            if (!_isDragging) HandleCameraPosition();
+            HandleCameraZoom();
+        }
+
+        private void HandleDrag()
+        {
+            if (_dragAction.triggered)
+            {
+                StartDragging();
+            }
+            else if (_dragAction.ReadValue<float>() > 0)
+            {
+                DragCamera();
+            }
+
+            if (_dragAction.WasReleasedThisFrame())
+            {
+                _isDragging = false;
+            }
+        }
+
+        private void StartDragging()
+        {
+            Vector3 point = GetPoint();
+            if (Vector3.Distance(point, transform.position) > 3f)
+            {
+                Vector3 targetCameraPosition = transform.position - _forward * _currentCameraHeight;
+
+                _isDragging = true;
+
+                _dragAngle = Mathf.Atan2(transform.position.z - point.z, point.x - transform.position.x) *
+                             Mathf.Rad2Deg + 90;
+
+                _dampedOffsetVector = new Vector2(
+                    _dampedCameraPosition.x - targetCameraPosition.x + _dampedOffsetVector.x,
+                    _dampedCameraPosition.z - targetCameraPosition.z + _dampedOffsetVector.y);
+            }
+        }
+
+        private void DragCamera()
+        {
+            Vector3 point = GetPoint();
+            if (_isDragging && Vector3.Distance(point, transform.position) > 0.25f)
+            {
+                float dragAngle = Mathf.Atan2(transform.position.z - point.z, point.x - transform.position.x) *
+                                  Mathf.Rad2Deg + 90;
+
+                float deltaAngle = _dragAngle - dragAngle;
+                Vector3 eulerAngles = playerCamera.transform.rotation.eulerAngles;
+                float targetAngle = eulerAngles.y + deltaAngle;
+                targetAngle = Mathf.MoveTowardsAngle(eulerAngles.y, targetAngle, Time.deltaTime * 360f);
+
+                playerCamera.transform.rotation =
+                    Quaternion.Euler(eulerAngles.x, targetAngle, eulerAngles.z);
+
+                _forward = playerCamera.transform.forward;
+            }
+        }
+
+        private void HandleCameraPosition()
+        {
+            Vector3 point = GetPoint();
+            Vector2 distanceToPlayer = new Vector2(point.x, point.z) -
+                                       new Vector2(transform.position.x, transform.position.z);
+
+            distanceToPlayer = Vector2.ClampMagnitude(distanceToPlayer, maxOffset);
+            _dampedOffsetVector = Vector2.SmoothDamp(_dampedOffsetVector, distanceToPlayer,
+                ref _currentDistanceVectorVelocity, offsetDampTime);
+
+            _dampedCameraPosition = Vector3.SmoothDamp(_dampedCameraPosition,
+                new Vector3(transform.position.x + cameraPositionOffset.x, transform.position.y, transform.position.z + _currentCameraVertical) - _forward * _currentCameraHeight, ref _cameraVelocity, cameraDampTime);
+
+            playerCamera.transform.position = _dampedCameraPosition + new Vector3(_dampedOffsetVector.x, 0, _dampedOffsetVector.y);
+        }
+
+        private void HandleCameraZoom()
+        {
+            float scrollDelta = _cameraZoomAction.ReadValue<Vector2>().y;
+            if (scrollDelta == 0) return;
+
+            float heightDifference = scrollDelta < 0f ? cameraZoomPower : -cameraZoomPower;
+            _cameraHeightTarget = _currentCameraHeight + heightDifference;
+            _cameraVerticalTarget = _currentCameraVertical + heightDifference;
+            _cameraHeightTarget = Mathf.Clamp(_cameraHeightTarget, minCameraHeight, maxCameraHeight);
+            _cameraVerticalTarget = Mathf.Clamp(_cameraVerticalTarget, minCameraVertical, maxCameraVertical);
+
+            LerpCameraHeight();
+        }
+
+        private void LerpCameraHeight()
+        {
+            _currentCameraHeight = Mathf.Lerp(_currentCameraHeight, _cameraHeightTarget, Time.deltaTime * cameraZoomSpeed);
+            _currentCameraVertical = Mathf.Lerp(_currentCameraVertical, _cameraVerticalTarget, Time.deltaTime * cameraZoomSpeed);
+        }
+
+        private Vector3 GetPoint()
+        {
+            var playerPlane = new Plane(Vector3.up, transform.position);
+            var mousePosition = Mouse.current.position.ReadValue();
+            var ray = playerCamera.ScreenPointToRay(mousePosition);
+            return playerPlane.Raycast(ray, out var hitDist) ? ray.GetPoint(hitDist) : Vector3.zero;
+        }
+    }
+}
---
+++ Assets/_Systems/PlayerMovement/TopDownMovementController.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PlayerMovement
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class TopDownMovementController : MonoBehaviour
+    {
+        // INPUT
+        public InputActionAsset inputActionAsset;
+        private InputAction _moveAction;
+        private InputAction _jumpAction;
+
+        // NAVIGATION
+        public bool movementEnabled = true;
+        public float jumpHeight = 4;
+        public float gravity = 10;
+        public float moveSpeed = 5;
+
+        private CharacterController _characterController;
+        private Animator _anim;
+        private Vector3 _displacement;
+        private float _verticalSpeed;
+        private bool _isJumping;
+        private float _actualMoveSpeed;
+
+        // ANIMATOR
+        static readonly int HorizontalHash = Animator.StringToHash("Horizontal");
+        static readonly int VerticalHash = Animator.StringToHash("Vertical");
+        static readonly int FallingHash = Animator.StringToHash("Falling");
+        static readonly int JumpHash = Animator.StringToHash("Jump");
+        public float animatorSmoothTime = 0.15f;
+
+        // OTHER
+        private Vector2? _targetPosition;
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+            _actualMoveSpeed = moveSpeed;
+            InitializeInputSystem();
+            
+            _mainCamera = Camera.main;
+        }
+
+        private void InitializeInputSystem()
+        {
+            _moveAction = inputActionAsset.FindAction("Move");
+            _jumpAction = inputActionAsset.FindAction("Jump");
+            _moveAction.Enable();
+            _jumpAction.Enable();
+        }
+
+        private void Update()
+        {
+            if (movementEnabled) HandleMovement();
+        }
+
+        public void SetAnimator(Animator animator)
+        {
+            _anim = animator;
+        }
+
+        private void HandleMovement()
+        {
+            _displacement = Vector3.zero;
+
+            if (_characterController.isGrounded)
+            {
+                if (_jumpAction.triggered) HandleJump();
+                else HandleGroundedMovement();
+            }
+            else
+            {
+                HandleAirborneMovement();
+            }
+
+            RotateCharacterToCursor();
+            ApplyGravity();
+            _characterController.Move(_displacement);
+        }
+
+        private void HandleJump()
+        {
+            _targetPosition = null;
+            _verticalSpeed = jumpHeight;
+            _isJumping = true;
+            _anim?.SetTrigger(JumpHash);
+        }
+
+        private void HandleGroundedMovement()
+        {
+            _verticalSpeed = 0f;
+            _isJumping = false;
+
+            _anim?.SetBool(FallingHash, false);
+            
+            Vector2 input = _moveAction.ReadValue<Vector2>();
+            input.Normalize();
+
+            // Transform input to be relative to the camera's direction
+            Vector3 forward = _mainCamera.transform.forward;
+            Vector3 right = _mainCamera.transform.right;
+
+            forward.y = 0; // Ensure we're only moving in XZ plane
+            right.y = 0; // Ensure we're only moving in XZ plane
+
+            Vector3 moveDirection = (forward * input.y + right * input.x).normalized;
+            moveDirection *= (_actualMoveSpeed * Time.deltaTime);
+
+            if (_targetPosition is { } value)
+            {
+                Vector2 displacementToTarget = value - new Vector2(transform.position.x, transform.position.z);
+                Vector3 clampedMoveDirection = Vector2.ClampMagnitude(displacementToTarget, _actualMoveSpeed * Time.deltaTime);
+                moveDirection = new Vector3(clampedMoveDirection.x, 0, clampedMoveDirection.y);
+            }
+
+            if (moveDirection != Vector3.zero)
+            {
+                _displacement.x = moveDirection.x;
+                _displacement.z = moveDirection.z;
+                SetAnimatorParameters(new Vector2(moveDirection.x, moveDirection.z), true);
+            }
+            else
+            {
+                SetAnimatorParameters(Vector2.zero, false);
+            }
+        }
+
+        private void HandleAirborneMovement()
+        {
+            _targetPosition = null;
+            _isJumping = true;
+            float groundDistance = GetGroundDistance();
+
+            _anim?.SetBool(FallingHash, groundDistance > 0.2f);
+        }
+
+        private void ApplyGravity()
+        {
+            if (_characterController.isGrounded && !_isJumping)
+            {
+                _displacement.y = -gravity * Time.deltaTime;
+            }
+            else
+            {
+                _displacement.y = _verticalSpeed * Time.deltaTime;
+            }
+
+            _verticalSpeed -= gravity * Time.deltaTime;
+        }
+
+        private void RotateCharacterToCursor()
+        {
+            Vector3 point = GetPoint();
+            Vector3 direction = point - transform.position;
+            direction.y = 0; // Ensure we're only rotating on the Y axis
+
+            if (direction.sqrMagnitude > 0.1f) // Check for a valid direction to avoid jittering
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+            }
+        }
+
+        private void SetAnimatorParameters(Vector2 input, bool isMoving)
+        {
+            if (isMoving)
+            {
+                float horizontal = input.x;
+                float vertical = input.y;
+                _anim?.SetFloat(HorizontalHash, horizontal, animatorSmoothTime, Time.deltaTime);
+                _anim?.SetFloat(VerticalHash, vertical, animatorSmoothTime, Time.deltaTime);
+            }
+            else
+            {
+                _anim?.SetFloat(HorizontalHash, 0, animatorSmoothTime, Time.deltaTime);
+                _anim?.SetFloat(VerticalHash, 0, animatorSmoothTime, Time.deltaTime);
+            }
+        }
+
+        private float GetGroundDistance()
+        {
+            if (Physics.Raycast(transform.position, -Vector3.up, out var hit))
+            {
+                return hit.distance;
+            }
+            return 0;
+        }
+
+        private Vector3 GetPoint()
+        {
+            var playerPlane = new Plane(Vector3.up, transform.position);
+            var mousePosition = Mouse.current.position.ReadValue();
+            var ray = _mainCamera.ScreenPointToRay(mousePosition);
+            return playerPlane.Raycast(ray, out var hitDist) ? ray.GetPoint(hitDist) : Vector3.zero;
+        }
+    }
+}
